8592e2cb25f14f0687135aa4d45923c09517ebcd
Zijie Wu <noojeff@gmail.com>
2020-03-29 10:40:45 -0700

    Added google analy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `blog`,
+  pathPrefix: `/blog`,
   siteMetadata: {
     title: `Mindscape`,
     description: `Site description placeholder`,
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-114654492-1`,
       },
     },
     `gatsby-plugin-feed`,
