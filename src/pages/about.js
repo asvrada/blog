e4c221746e272d70cc227305a4b405ffa49aa333
Zijie Wu <noojeff@gmail.com>
2020-05-05 20:22:03 -0700

    Added iframe

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Iframe from "react-iframe";
 import { graphql } from "gatsby";
 
 import Layout from "../layout";
@@ -12,6 +13,8 @@ const BlogAbout = ({ data }) => {
       <SEO title="About"/>
       <h1>About Page</h1>
       <p>Under construction</p>
+      <Iframe id="if-imagewall" className="p-0 m-0"
+              url="https://asvrada.github.io/vue-imagewall/"/>
     </Layout>
   );
 };
