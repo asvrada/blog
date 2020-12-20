18fd8fd6a0820049e4be4cf330974c136a2b4dc9
Zijie Wu <noojeff@gmail.com>
2020-12-20 00:39:21 -0800

    Add temp fix for prod

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -50,7 +50,8 @@ const Posts = ({ data, location, navigate }) => {
                          options={["All", "Code", "Note", "Life"]}
                          selection={selection}
                          onSelectionChange={(newSelection) => {
-                           navigate(`posts/?filter=${INVERSE_SELECTION_TO_TYPE[newSelection]}`);
+                           // todo: fix for dev and prod
+                           navigate(`/blog/posts/?filter=${INVERSE_SELECTION_TO_TYPE[newSelection]}`);
                            setSelection(newSelection);
                          }}
         />
